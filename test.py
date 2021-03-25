7249848a156d0af0e8499086e83c33564eb5b497
Dreamlittlecat <76044518+Dreamlittlecat@users.noreply.github.com>
2021-03-25 15:12:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,5 @@ if pid == 0:
     print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
 else:
     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+exit()
+    
